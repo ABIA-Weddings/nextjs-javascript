048f4f11c7d8668eb413909fc7ba1e2543145152
NaufalMZ05 <mohamed@abia.com.au>
2025-01-20 11:34:12 +0530

    testing

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ const About: FC = () => {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
       <h1>About Us</h1>
+      
       <p>
         Welcome to our About Us page! We are a dedicated team committed to
         delivering the best experiences for our customers. Our goal is to
